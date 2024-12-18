d5a4bacd9c6428582223eabda351a2681ba6c90f
luputer <m.saidibjm4@gmail.com>
2024-12-18 16:24:18 +0800

    kode program untuk melakukan sorting asc

--- menu.cpp
+++ menu.cpp
@@ -2,62 +2,99 @@
 #include <conio.h>
 using namespace std;
 
+void dMenu(); // Function declarations
+void mInputData();
+void mTampilkanData();
+void sortingAsc();
+void sortingDsc();
+void tukar(int *a, int *b);
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
+int n; // Global variable to store number of elements
+int myData[100];
 
-}
-
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
-
-
-int main() {
-char pl;
-do
+void dMenu()
 {
-    dMenu();
-    pl=getch();
-  switch (pl)
+  system("cls");
+  cout << "Aplikasi Tampilan Menu" << "\n";
+  cout << "1. Menu Pertama" << "\n";
+  cout << "2. Menu Kedua" << "\n";
+  cout << "3. Menu Ketiga" << "\n";
+  cout << "4. Menu Keempat" << "\n";
+  cout << "5. Exit" << "\n";
+  cout << "Masukan angka :";
+}
+
+void mPertama(string pesan)
+{
+  system("cls");
+  cout << "hallo saya menu " << pesan;
+  getch();
+}
+
+void mTampilkanData()
+{
+  system("cls");
+  cout << "Data yang dimasukkan: ";
+  for (int i = 0; i < n; i++)
   {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
+    cout << myData[i] << " ";
   }
+  cout << "\n";
+  getch();
+}
 
+void sortingAsc()
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      if (myData[j] > myData[j + 1])
+      {
+        tukar(&myData[j], &myData[j + 1]);
+      }
+    }
+  }
+  cout << "Data telah diurutkan dalam urutan menaik.\n";
+  getch();
+}
 
-} while (pl!='5');
+int main()
+{
+  char pl;
+  do
+  {
+    dMenu();
+    pl = getch();
+    switch (pl)
+    {
+    case '1':
+      /* code */
+      mPertama("pertama");
+      break;
+    case '2':
+      mPertama("ke- dua");
+      /* code */
+      break;
+    case '3':
+      mPertama("ke- tiga");
+      /* code */
+      break;
+    case '4':
+      mPertama("ke- empat");
+      /* code */
+      break;
+    case '5':
+      /* code */
+      break;
+
+    default:
+      system("cls");
+      cout << "Pilihan Tidak Tersedia";
+      getch();
+      break;
+    }
+
+  } while (pl != '5');
   return 0;
 }
